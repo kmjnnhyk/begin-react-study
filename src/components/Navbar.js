084f0b38e1e41e66be890a411c0852e4ebc02d0c
kmjnnhyk <rlawlsgur620@gmail.com>
2021-08-24 17:16:17 +0900

    Navbar 반응형 작업 완료

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,26 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from './Button';
+import './Navbar.scss';
 
 function Navbar() {
   const [click, setClick] = useState(false);
   const [button, setButton] = useState(true);
+  const [menuToggle, setMenuToggle] = useState(false);
 
   const handleClick = () => setClick(!click);
-  const closeMobileNav = () => setClick(!click);
+  const closeMobileNav = () => setClick(false);
   const showButton = () => {
     if (window.innerWidth <= 960) { setButton(false); }
     else { setButton(true); }
   };
+  const showMenuToggle = () => {
+    if (window.innerWidth <= 960) { setMenuToggle(true); }
+    else { setMenuToggle(false); }
+  }
 
   window.addEventListener('resize', showButton);
+  window.addEventListener('resize', showMenuToggle);
 
   return (
     <>
@@ -22,12 +29,13 @@ function Navbar() {
           <Link to='/' className='navbar-logo'>
             LOGO<i className='far fa-image' />
           </Link>
-          <div className='menu-icon' onClick={handleClick}>
-            <i className={click ?
-              'fas fa-times' : 'fas fa-bars'
-            } />
-          </div>
-          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+          {menuToggle &&
+            <div className='menu-icon' onClick={handleClick}>
+              <i className={click ?
+                'fas fa-times' : 'fas fa-bars'
+              } />
+            </div>}
+          <ul className={click && menuToggle ? 'nav-menu active' : 'nav-menu'}>
             <li className='nav-item'>
               <Link to='/' className='nav-link' onClick={closeMobileNav}>
                 HOME
@@ -43,11 +51,12 @@ function Navbar() {
                 PRODUCTS
               </Link>
             </li>
-            <li className='nav-item'>
-              <Link to='/sign-up' className='nav-link-mobile' onClick={closeMobileNav}>
-                SIGN-UP
-              </Link>
-            </li>
+            {!button &&
+              <li className='nav-item'>
+                <Link to='/sign-up' className='nav-link-mobile' onClick={closeMobileNav}>
+                  SIGNUP
+                </Link>
+              </li>}
           </ul>
           {button &&
             <Button buttonStyle='btn--outline'>SIGN-UP</Button>
