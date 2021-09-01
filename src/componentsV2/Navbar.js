f2e35f432ef6d4fb4009039cd56b0266ce91f55f
kmjnnhyk <rlawlsgur620@gmail.com>
2021-09-01 23:36:26 +0900

    Navbar 반응형 작업 및 간단한 animation 구현

--- src/componentsV2/Navbar.js
+++ src/componentsV2/Navbar.js
@@ -1,46 +1,63 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './scss/Navbar.scss';
 
 function Navbar() {
+  const [toggle, setToggle] = useState(false); // below 960px -> toggle == true
+  const [mobile, setMobile] = useState(false); // toggle mobile menu
 
-  function toggleLogoAnimation() {
-    const logo = document.querySelector('.logo-text');
-    logo.classList.toggle('logo-animation', window.innerWidth < 960);
-  }
+  const handleToggle = () => {
+    if (window.innerWidth <= 960) {
+      setToggle(true);
 
-  window.addEventListener('resize', toggleLogoAnimation);
+    }
+    else {
+      setToggle(false);
+    }
+
+  };
+  const handleMobile = () => { setMobile(prev => !prev) };
+  const closeMobile = () => {
+    setMobile(false);
+  };
+
+  useEffect(() => {
+    handleToggle();
+  });
+
+  window.addEventListener('resize', handleToggle);
 
   return (
-    <>
+    <div className='navbar'>
       <div className='navbar-container'>
         <div className="logo">
-          <Link to='/Home' className='menu-link'>
+          <Link to='/Home' className='logo-link' onClick={closeMobile}>
             <h1 className='logo-text'>KMJNHK</h1>
           </Link>
         </div>
-        <ul className="menu-list">
+
+        <ul className={mobile ? 'menu-list mobile' : 'menu-list'}>
           <li className="menu">
-            <Link to='/Works' className='menu-link'>
+            <Link to='/Works' className='menu-link' onClick={closeMobile}>
               WORKS
             </Link>
           </li>
           <li className="menu">
-            <Link to='/About' className='menu-link'>
+            <Link to='/About' className='menu-link' onClick={closeMobile}>
               ABOUT
             </Link></li>
           <li className="menu">
-            <Link to='/Contacts' className='menu-link'>
+            <Link to='/Contacts' className='menu-link' onClick={closeMobile}>
               CONTACTS
             </Link></li>
         </ul>
-        <div className="menu-toggle">
-          <i className="fas fa-bars"></i>
-        </div>
+        {toggle &&
+          <div className="menu-toggle" onClick={handleMobile}>
+            <i className="fas fa-bars"></i>
+          </div>}
+
       </div>
-      <section>d</section>
-      <section>d</section>
-    </>
+    </div>
   );
 }
 
